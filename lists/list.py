5fd4f269011853a8a19b7aad62d22fdf24d03c98
Gurnav224 <chaudharyg856@gmail.com>
2025-07-23 00:02:14 +0000

    learn about python dictionary and list

--- lists/list.py
+++ lists/list.py
@@ -1,3 +1,32 @@
 mylist = ["apple", "banana", "cherry"]
 
-print(mylist)
+# print(mylist)
+
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+
+# len to print the length of the list
+
+# print(len(thislist))
+
+
+# list can store the string, number , boolean value
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+# list are type of object
+
+# print(type(list))e
+
+
+# we can create new list using list constructor
+
+myL = list(("apple", "banana", "cherry"))
+# print(myL)
+
+
+for item in mylist:
+    print(item)
