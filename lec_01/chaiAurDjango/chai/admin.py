ff8916074ed21679c4c7775634b0cab99ea05136
Gurnav224 <chaudharyg856@gmail.com>
2025-08-10 10:39:30 +0000

    django relationship model

--- lec_01/chaiAurDjango/chai/admin.py
+++ lec_01/chaiAurDjango/chai/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity, ChaiCertificate, ChaiReview, Store
+
+
 # Register your models here.
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+    
 
-
-admin.site.register(ChaiVarity)
-
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name','type','date_added')
+    inlines = [ChaiReviewInline]
+    
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('chai_varities',)
+    
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai','certificate_number')
+    
+    
+admin.site.register(ChaiVarity,ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
